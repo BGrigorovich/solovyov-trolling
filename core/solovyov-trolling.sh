5c495362d561387fb2d26250191a5f4a7218503c
Bogdan Grigorovich <bohdan.hryhorovych@gmail.com>
2016-01-28 01:13:24 +0200

    Replaced argparse with click

--- core/solovyov-trolling.sh
+++ core/solovyov-trolling.sh
@@ -2,4 +2,4 @@
 
 source /home/bogdan/.virtualenvs/twitter_env/bin/activate
 cd /home/bogdan/PycharmProjects/solovyov-trolling/core/
-python twitter.py -f replies.txt
+python twitter.py -f replies.txt -a auth_credentials.json -u bgrigorovich
--- core/twitter.py
+++ core/twitter.py
@@ -2,22 +2,14 @@
 import json
 import random
 import codecs
-import argparse
 import logging
 import tweepy
+import click
 
 
 def get_replies(replies_file_name):
-    with codecs.open(replies_file_name, encoding='utf-8', errors='skip') as replies_file:
-        return [reply for reply in replies_file]
-
-
-def get_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--file', help='File with list of replies', required=True)
-    parser.add_argument('-a', '--auth_credentials', help='Authentication credentials', required=True)
-    parser.add_argument('-u', '--user', help='User that will receive replies', required=False)
-    return vars(parser.parse_args())
+    with codecs.open(replies_file_name, encoding='utf-8', errors='skip') as replies:
+        return [reply for reply in replies]
 
 
 def get_api(auth_credentials_file_name):
@@ -31,29 +23,34 @@ def get_api(auth_credentials_file_name):
 
 
 class MyStreamListener(tweepy.StreamListener):
-    def __init__(self, user, replies):
+    def __init__(self, api, user, replies):
         super(MyStreamListener, self).__init__()
         self.user = user
         self.replies = replies
+        self.api = api
 
     def on_status(self, status):
         if not hasattr(status, 'retweeted_status') and not status.in_reply_to_status_id:
             logging.info('replying to tweet #{0}'.format(status.id))
-            api.update_status('@{0}\n{1}'.format(self.user.screen_name, random.choice(self.replies)), status.id)
+            self.api.update_status('@{0}\n{1}'.format(self.user.screen_name, random.choice(self.replies)), status.id)
 
 
-if __name__ == '__main__':
+@click.command()
+@click.option('-f', '--replies_file', help='File with list of replies')
+@click.option('-a', '--auth_credentials', help='Authentication credentials')
+@click.option('-u', '--user', help='User that will receive replies', default='dan_1k')
+def main(replies_file, auth_credentials, user):
     logging.basicConfig(filename='info.log', level=logging.INFO,
                         format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s', datefmt='%a %b %d %H:%M:%S %Y')
     logging.info('start script')
 
-    auth_cred = get_args()['auth_credentials']
-    target_user_name = get_args()['user']
-    replies = get_replies(get_args()['file'])
+    api = get_api(auth_credentials)
+    replies = get_replies(replies_file)
+    target_user = api.get_user(user)
 
-    api = get_api(auth_cred)
+    stream_listener = MyStreamListener(api, target_user, replies)
+    stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
+    stream.filter([str(target_user.id)])
 
-    target_user = api.get_user(target_user_name or 'dan_1k')
-    myStreamListener = MyStreamListener(target_user, replies)
-    myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
-    myStream.filter([str(target_user.id)])
+if __name__ == '__main__':
+    main()
